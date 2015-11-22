c5b9d33ec2855d265ae7c0be0c5928cc65c74f34
yama@muteki <yama@muteki.com>
2015-11-22 21:25:37 +0900

    Add ticket.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
   root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
+
+  resources :events do
+    resources :tickets
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
